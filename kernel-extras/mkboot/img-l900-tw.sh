decde2050695e589054f1c3d52bdf750d796278d
garwynn <byon425@yahoo.com>
2013-01-18 16:35:45 -0600

    Test ramdisk chg

--- kernel-extras/mkboot/img-l900-tw.sh
+++ kernel-extras/mkboot/img-l900-tw.sh
@@ -6,5 +6,5 @@ echo "making ramdisk"
 
 echo "making boot.img"
 # packs the ramdisk together with the zImage at the proper locations and makes the boot.img
-./mkbootimg --cmdline "console=null androidboot.hardware=qcom user_debug=31" --kernel $PWD/zImage --ramdisk $PWD/ramdisk.gz --base 0x10000000 --pagesize 2048 --ramdiskaddr 0x11000000 -o $PWD/boot.img
+./mkbootimg --cmdline "console=null androidboot.hardware=qcom user_debug=31" --kernel $PWD/zImage --ramdisk $PWD/ramdisk.gz --base 0x80200000 --ramdiskaddr 0x81500000 -o $PWD/boot.img
 rm ramdisk.gz
